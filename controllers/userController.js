d03ae6c24a3b6a7360338963b35f20e2d4655f67
Jonathan Young <jonathanyoung@Jonathans-MacBook-Pro.local>
2024-02-28 17:42:45 -0500

    added error handling responses for users and training plans

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const { generateToken, checkPassword, getRefreshToken } = require('../helpers/he
 const { jwtDecode } = require('jwt-decode')
 const User = require('../models/user.js');
 const Token = require('../models/token.js')
-const { handleErrors } = require('../helpers/handleErrors.js')
+const { handleUserErrors } = require('../helpers/handleErrors.js')
 const { resetPasswordEmail } = require('../helpers/resetPasswordEmail.js')
 const bcrypt = require('bcrypt')
 
@@ -13,16 +13,18 @@ module.exports.login = async (req, res) => {
         const user = await User.findOne({ email })
         
         if (!user) {
-            return res.status(401).json({
-                message: 'User not found!'
+            return res.status(401).send({
+                email: 'User not found!',
+                password: ''
             })
         }
 
         const isPasswordValid = await checkPassword(password, user.password)
 
         if (!isPasswordValid) {
-            return res.status(401).json({
-                message: 'Invalid password!'
+            return res.status(401).send({
+                email: '',
+                password: 'Incorrect password!'
             })
         }
 
@@ -40,46 +42,55 @@ module.exports.login = async (req, res) => {
             refreshToken
         })
     } catch (err) {
-        const errors = handleErrors(err)
+        const errors = handleUserErrors(err)
         res.status(400).json({ errors });
     }
 }
 
 module.exports.signup = async (req, res) => {
-    const { email, password, firstName, lastName } = req.body
+    try {
+        const { email, password, firstName, lastName } = req.body
 
-    const userExists = await User.find({ email: email }).lean()
-
-    if (userExists.length > 0) {
-        return res.status(400).json({
-            message: 'Email already exists'
-        })
+        const userExists = await User.find({ email: email }).lean()
+    
+        if (userExists.length > 0) {
+            return res.status(400).send({
+                firstName: '',
+                lastName: '',
+                password: '',
+                email: 'Email already exists'
+            })
+        }
+    
+        const newUser = {
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            password: password,
+            privilige: 1
+        }
+    
+        const user = new User(newUser)
+        const savedUser = await user.save()
+    
+        if (savedUser) {
+            const accessToken = generateToken(savedUser)
+            const decodedToken = jwtDecode(accessToken)
+            const accessTokenExpiry = decodedToken.exp
+            const refreshToken = getRefreshToken(savedUser)
+    
+            return res.status(200).json({
+                message: 'User created successfully',
+                accessToken,
+                expiresAt: accessTokenExpiry,
+                refreshToken
+            })
+        }
+    } catch (err) {
+        const errors = handleUserErrors(err)
+        res.status(400).json({ errors });
     }
-
-    const newUser = {
-        email: email,
-        firstName: firstName,
-        lastName: lastName,
-        password: password,
-        privilige: 1
-    }
-
-    const user = new User(newUser)
-    const savedUser = await user.save()
-
-    if (savedUser) {
-        const accessToken = generateToken(savedUser)
-        const decodedToken = jwtDecode(accessToken)
-        const accessTokenExpiry = decodedToken.exp
-        const refreshToken = getRefreshToken(savedUser)
-
-        return res.status(200).json({
-            message: 'User created successfully',
-            accessToken,
-            expiresAt: accessTokenExpiry,
-            refreshToken
-        })
-    }
+    
 }
 
 module.exports.refresh_token = async (req, res) => {
@@ -122,12 +133,12 @@ module.exports.forgot_password = async (req, res) => {
         .then(async (result) => {
             await resetPasswordEmail(result.email, result.firstName, result.lastName, resetCode)
 
-            return res.status(200).send({ result: true, message: 'If the user exists, an email was sent'})
+            return res.status(200).send({ email: 'If the user exists, an email was sent'})
         }).catch((err) => {
-            return res.status(400).send({ result: false, message: 'If the user exists, an email was sent'})
+            return res.status(400).send({ email: 'If the user exists, an email was sent'})
         })
     } catch(err) {
-        return res.send({ result: false, message: 'If the user exists, an email was sent'})
+        return res.send({ email: 'If the user exists, an email was sent'})
     }
 }
 
@@ -138,11 +149,11 @@ module.exports.reset_password = async (req, res) => {
         User.findOne({email: email})
         .then(async (result) => {
             if (!result || result.resetCode != resetCode) {
-                return res.status(401).send({ result: false, message: 'Please try to reset your password again'})
+                return res.status(401).send({ resetCode: 'Incorrect verification code!', password: '', confirmPassword: ''})
             }
 
             if (result.resetCodeExpiration < new Date()) {
-                return res.status(402).send({ result: false, message: 'Token has expired, please try again!'})
+                return res.status(402).send({ resetCode: 'Token has expired, please try again!', password: '', confirmPassword: ''})
             }
 
             const hashedPassword = await bcrypt.hash(password, 10)
@@ -151,13 +162,13 @@ module.exports.reset_password = async (req, res) => {
             .then(async (result) => {
                 return res.status(200).send({ result: true, message: 'Password reset!'})
             }).catch((err) => {
-                return res.status(403).send({ result: false, message: 'An error occured resetting your password. Please try again!'})
+                return res.status(403).send({ resetCode: 'An error occured resetting your password. Please try again!', password: '', confirmPassword: ''})
             })
         }).catch((err) => {
-            res.status(400).send({ result: false, message: 'No user with that email'})
+            res.status(400).send({ resetCode: 'No user with that email', password: '', confirmPassword: ''})
         })
     } catch (err) {
-        return res.status(500).send({result: false, message: 'An error occured resetting your password. Please try again!'})
+        return res.status(500).send({resetCode: 'An error occured resetting your password. Please try again!', password: '', confirmPassword: ''})
     }
 }
 
