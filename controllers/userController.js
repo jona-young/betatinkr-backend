c01128214fe6e7d3517de9057f3b2302b6493d51
Jonathan Young <jonathanyoung@Jonathans-MacBook-Pro.local>
2024-03-11 14:57:42 -0400

    added user delete backend route and controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,6 @@ const Token = require('../models/token.js')
 const { handleUserErrors } = require('../helpers/handleErrors.js')
 const { resetPasswordEmail } = require('../helpers/resetPasswordEmail.js')
 const bcrypt = require('bcrypt')
-const { floatGetter } = require('../helpers/floatGetter.js')
 
 module.exports.login = async (req, res) => {
     const { email, password } = req.body
@@ -92,6 +91,36 @@ module.exports.signup = async (req, res) => {
         res.status(400).json({ errors });
     }
     
+}
+
+module.exports.get_user = async (req, res) => {
+    const uid = req.auth.uid
+
+    try {
+        User.findById(uid)
+        .then((result) => {
+            res.status(200).json({ firstName: result.firstName, lastName: result.lastName, email: result.email})
+        }).catch((err) => {
+            res.status(400).json({ err });
+        })
+    } catch (err) {
+        return res.status(500).json({message: 'Could not get user information!'})
+    }
+}
+
+module.exports.delete_user = async (req, res) => {
+    const uid = req.auth.uid
+
+    try {
+        User.findByIdAndDelete(uid)
+        .then((result) => {
+            res.status(200).send({ message: 'User successfully deleted!'})
+        }).catch((err) => {
+            res.status(400).send({ message: 'User was not found!'})
+        })
+    } catch (err) {
+        return res.status(500).json({message: 'Could not get user information!'})
+    }
 }
 
 module.exports.refresh_token = async (req, res) => {
@@ -195,7 +224,7 @@ module.exports.activity_template = async (req, res) => {
             res.status(err)
         })
     } catch (err) {
-        res.status(400).json({ errors });
+        res.status(400).json({ err });
     }
 }
 
@@ -261,192 +290,3 @@ module.exports.delete_activity_template = async (req, res) => {
         res.status(err)
     })
 }
-
-// // DELETE :id
-// module.exports.user_delete = (req, res) => {
-//     const token = req.cookies.jwt;
-//     const id = req.params.id;
-
-//      // Check jwt validation
-//      if (token) {
-//          jwt.verify(token, process.env.JWTSECRET, (err, decodedToken) => {
-//              if (err) {
-//                  res.status(400).send({ errors: {jwt: "Invalid JWT token"}})
-//              } else {
-//                 User.findByIdAndDelete(id)
-//                 .then((result) => {
-//                     res.status(200).cookie('jwt', '', { httpOnly: true, maxAge: 1 }).send(result);
-//                 }).catch((err) => {
-//                     res.status(400)
-//                 })
-//              }
-//          })
-//      }    
-// }
-
-// module.exports.get_user = (req, res) => {
-//     const id = req.params.id
-//     const token = req.cookies.jwt;
-
-//     // Check jwt validation
-//     if (token) {
-//         jwt.verify(token, process.env.JWTSECRET, async (err, decodedToken) => {
-//             if (err) {
-//                 res.status(400).send({ errors: {jwt: "Invalid JWT token"}})
-//             } else {
-//                 User.findById(id)
-//                 .then((result) => {
-//                     res.status(200).json({ firstName: firstName, lastName: lastName, token: token, _id: user._id });
-//                 }).catch((err) => {
-//                     res.status(err);
-//                 })  
-//             }
-//         })
-//     }  
-// }
-
-// module.exports.put_user = (req, res) => {
-//     const id = req.params.id;
-//     const body = req.body;
-//     const token = req.cookies.jwt;
-
-//      // Check jwt validation
-//      if (token)
-//      {
-//          jwt.verify(token, process.env.JWTSECRET, async (err, decodedToken) => {
-//              if (err) {
-//                  res.status(400).send({ errors: {jwt: "Invalid JWT token"}})
-//              } else {
-//                 User.findByIdAndUpdate(id, body)
-//                 .then((result) => {
-//                     res.status(200).send({ result: 'Updated!'})
-//                 }).catch((err) => {
-//                     res.status(400);
-//                 })
-//              }
-//          })
-//      }   
-// }
-
-// module.exports.forgot_password = async (req, res) => {
-//     if (req.body.email !== undefined)
-//     {
-//         let emailAddress = req.body.email
-
-//         User.findOne({ email: emailAddress})
-//         .then(async (userAcc) => {
-//             let payload = {
-//                 id: userAcc._id,
-//                 email: emailAddress
-//             }
-
-//             const token = createToken(payload,  userAcc.password + '-' + userAcc.privilige)
-
-//             // send email out
-//             await resetPasswordEmail(emailAddress, userAcc.firstName, userAcc.lastName, userAcc._id, token)
-
-//             res.status(200).send({response: { email: 'Password reset email sent!'}})
-//         }).catch((err) => {
-//             res.status(400).send({response: { email: 'User not found!'}})
-//         })
-//     }
-//     else { res.status(400).send({ response: {email: 'Email address not associated with an account'}})}
-// }
-
-// module.exports.forgot_password_check = (req, res) => {
-//     const id = req.params.id
-//     const _token = req.params.token
-
-//     User.findOne({ _id: id})
-//     .then((userAcc) => {
-//         let payload = jwt.decode(_token, userAcc.password + '-' + userAcc.createdAt.getTime())
-
-//         res.status(200).send({
-//             id: payload.id,
-//             token: _token
-//         })
-//     }).catch((err) => {
-//         res.send(400)
-//     })
-// }
-
-// module.exports.reset_password = (req, res) => {
-//     const id = req.body.id
-//     const _token = req.body.token
-//     const { password1, password2 } = req.body
-
-//     if ( password1 !== password2)
-//     {
-//         res.status(400).send({ errors: { password: "Passwords do not match!"}})
-//     }
-//     else
-//     {
-//         User.findOne({ _id: id})
-//         .then(async (userAcc) => {
-//             let payload = jwt.decode(_token, userAcc.password + '-' + userAcc.privilige)
-    
-//             const salt = await bcrypt.genSalt();
-//             const hashedPW = await bcrypt.hash(password1, salt);
-
-//             User.findByIdAndUpdate({ _id: payload.id.id }, { password: hashedPW})
-//             .then(() => {
-//                 res.status(200).send({ result: "Password updated!"})
-//             }).catch((err) => {
-//                 res.status(400).send({ errors: { password: "User profile updating was not successful!" }})
-//             })
-//         }).catch((err) => {
-//             res.status(400).send({ errors: { password: "User not found!" }})
-//         })
-//     }
-// }
-
-// module.exports.contactus_post = async (req, res) => {
-//     const body = req.body;
-    
-//     const transporter = nodemailer.createTransport({
-//         host: "smtp.gmail.com",
-//         port: 587,
-//         secure: false,
-//         auth: {
-//             user: process.env.EMAIL_USER,
-//             pass: process.env.EMAIL_PW
-//         }
-//     })
-
-//     const info = await transporter.sendMail({
-//         from: process.env.EMAIL_USER,
-//         to: process.env.EMAIL_USER,
-//         subject: "Request - SUGGESTION",
-//         html: `
-//         <html>
-//             <head>
-//             </head>
-//             <body>
-//                 <p>
-//                     You have received a suggestion from  ${body.firstName + " " + body.lastName},
-//                     <br>
-//                     <br>
-//                     The message is as follows!
-//                     <br>
-//                     <br>
-//                     ${body.message}
-//                     <br>
-//                     <br>
-//                     Regards,
-//                     <br>
-//                     <br>
-//                     <b>Scrapn Support(AUTOMATED)</b>
-//                     <br>
-//                     E: <a href="mailto:scrapn.services@gmail.com">scrapn.services@gmail.com</a>
-//                 </p>
-//             </body>
-//         </html>
-//         `
-//     })
-
-//     if (info)
-//     {
-//         res.status(200).send({ result: "message received!"})
-//     }
-
-// }
